f659ef873fa87fa7b1c46c277c8e676a7a2332bd
shivam8899 <156087565+shivam8899@users.noreply.github.com>
2024-08-25 20:39:14 +0000

    variables examples

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,6 @@
+const accountid =6677
+let accountemail ="shivam@google.com"
+var accountpassword ="12434"
+accountcity ="jaipur"
+console.log(accountid)
+console.table([accountemail,accountpassword,accountcity])
